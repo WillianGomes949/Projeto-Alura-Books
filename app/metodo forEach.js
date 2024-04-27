b9c45b309230680bd1c230d2c232fc3e96fc6010
WillianGomes949 <williangomes949@gmail.com>
2024-04-27 02:26:23 -0300

    Mudanças no fileter e cração do map e reduce

---
+++ app/metodo forEach.js
@@ -0,0 +1,24 @@
+const elementoParaInserirLivros = document.getElementById('livros')
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis')
+
+function exibirOsLivrosNaTela(listaDeLivros) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = ''
+    elementoParaInserirLivros.innerHTML = ''
+    listaDeLivros.forEach(livro => {
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens': 'livros__imagens indisponivel'
+        let valor = disponibilidade == 'livros__imagens indisponivel' ? livro.preco.innerHTML = 'Indisponível': 'R$ ' + livro.preco.toFixed(2)
+        elementoParaInserirLivros.innerHTML += 
+        `<div class="livro">
+            <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
+            <h2 class="livro__titulo">
+            ${livro.titulo}
+            </h2>
+            <p class="livro__descricao">${livro.autor}</p>
+            <p class="livro__preco" id="preco">${valor}</p>
+            <div class="tags">
+                <span class="tag">${livro.categoria}</span>
+            </div>
+        </div>`
+    })
+}
+
